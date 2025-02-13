847345617c19d14cbb956988a2f1e6b78d97a7cf
minh.nghiem <minh.nghiem@mservice.com.vn>
2025-02-13 08:00:46 +0700

    page

---
+++ src/app/(store)/chinh-sach-bao-mat/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from "./page.module.scss";
+
+const Page = () => {
+  return (
+    <div className={`${styles.container} storeContainer`}>
+      <h1>Cam kết bán hàng</h1>
+      <p>
+        Cam kết bán hàng tại <strong>PIGINA</strong>
+      </p>
+      <ul>
+        <li>
+          Luôn cung cấp sản phẩm đúng chất lượng công bố đúng như hình ảnh và
+          thông tin đăng tải theo đúng mức giá được niêm yết.
+        </li>
+        <li>Cam kết tư vấn tận tình chu đáo.</li>
+        <li>
+          Xử lý các vấn đề phát sinh trên cơ sở đảm bảo quyền lợi tối đa cho
+          khách hàng.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
